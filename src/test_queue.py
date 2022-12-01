add3e1a6789eb93efffda05fdb07be2693e07fc3
Bjarke-M <bjarke.meyer.pedersen@gmail.com>
2022-12-01 18:35:41 +0100

    fingers crossed

--- src/test_queue.py
+++ src/test_queue.py
@@ -1,6 +1,20 @@
 """Testing queues."""
+from queue1 import Queue
 
 
 def test_me() -> None:
     """Remember to write tests."""
-    assert 2 + 2 == 4
+    assert 40 + 2 == 42
+
+
+def test_my_code() -> None:
+    queue_for_test = Queue()
+    assert queue_for_test.is_empty()==True
+    to_be_queue = [i for i in range(5,10,2)]
+    for i in to_be_queue:
+        queue_for_test.enqueue(i)
+    assert queue_for_test.front() == to_be_queue[0]
+    for i in to_be_queue:
+        assert i == queue_for_test.dequeue()
+
+
